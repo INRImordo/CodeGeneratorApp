c4b399e15bc044484211246acff49bfa2b7c7bce
INRImordo <inrimordo7@gmail.com>
2025-10-25 16:33:38 +0200

    comit01

--- m/v/view.py
+++ m/v/view.py
@@ -2,12 +2,35 @@
 import customtkinter as ctk
 from PIL import Image, ImageTk
 from tkinter import colorchooser
+from PIL import Image, ImageTk
+import os
+
 
 
 
 class CodeGeneratorView(ctk.CTk):
     def __init__(self):
         super().__init__()
+        
+        
+        
+        # ---- Chemins des icônes ----
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        ICON_COLOR = os.path.join(BASE_DIR, "icons", "emoji-sunglasses-fill.ico")
+        ICON_GEM = os.path.join(BASE_DIR, "icons", "gem.ico")
+        ICON_SAVE = os.path.join(BASE_DIR, "icons", "cloud-arrow-down-fill.ico")
+
+        # ---- Charger et redimensionner les icônes ----
+        self.icon_color = ImageTk.PhotoImage(
+            Image.open(ICON_COLOR).resize((20, 20), Image.Resampling.LANCZOS)
+        )
+        self.icon_gem = ImageTk.PhotoImage(
+            Image.open(ICON_GEM).resize((20, 20), Image.Resampling.LANCZOS)
+        )
+        self.icon_save = ImageTk.PhotoImage(
+            Image.open(ICON_SAVE).resize((20, 20), Image.Resampling.LANCZOS)
+        )
+        
         self.title("Générateur QR Code & Code-barres")
         self.geometry("600x690")
         self.resizable(False, False)
@@ -32,6 +55,9 @@ class CodeGeneratorView(ctk.CTk):
         )        
         self.entry_text.pack(pady=10)
         
+        ctk.set_default_color_theme("blue")
+        ctk.set_appearance_mode("light")
+        
 
         # Type de code
         self.code_type = ctk.StringVar(value="QR Code")
@@ -40,31 +66,31 @@ class CodeGeneratorView(ctk.CTk):
         self.radio_qr.pack(pady=5)
         self.radio_bar.pack(pady=5)
 
-        # Sélecteurs de couleur
-        self.color_frame = ctk.CTkFrame(self)
-        self.color_frame.pack(pady=10)
-
-        ctk.CTkLabel(self.color_frame, text="Couleur avant-plan :").grid(row=0, column=0, padx=10)
-        self.color_btn_fill = ctk.CTkButton(self.color_frame, text="Choisir", command=self.choose_fill_color)
-        self.color_btn_fill.grid(row=0, column=1, pady=5)
-        
-
-        ctk.CTkLabel(self.color_frame, text="Couleur arrière-plan :").grid(row=1, column=0, padx=10)
-        self.color_btn_back = ctk.CTkButton(self.color_frame, text="Choisir", command=self.choose_back_color)
-        self.color_btn_back.grid(row=1, column=1)
-        
-
-        # Bouton de génération
-        self.generate_button = ctk.CTkButton(self, text="Générer le code", width=200)
-        self.generate_button.pack(pady=15)
-
         # Zone d’affichage
         self.image_label = ctk.CTkLabel(self, text="")
         self.image_label.pack(pady=20)
 
-        # Bouton de sauvegarde
-        self.save_button = ctk.CTkButton(self, text="Enregistrer l'image", width=200)
-        self.save_button.pack(pady=10)
+        
+        
+        # Frame pour contenir les boutons côte à côte
+        self.buttons_frame = ctk.CTkFrame(self)
+        self.buttons_frame.pack(pady=15)
+        
+        # Bouton  0 : Choisir Couleur
+        self.color_btn_fill = ctk.CTkButton(self.buttons_frame, text="Choisir Couleur", width=200, command=self.choose_fill_color)
+        self.color_btn_fill.grid(row=0, column=1, pady=10)
+
+        # Bouton 1 : Générer
+        self.generate_button = ctk.CTkButton(self.buttons_frame, image=self.icon_gem, compound="left", text="Générer le code", width=200, fg_color="#007BFF",hover_color="#0056b3", text_color="white",border_color="#003f7f", border_width=2 )
+        self.generate_button.grid(row=0, column=0, padx=10)
+
+        # Bouton 2 : Enregistrer
+        self.save_button = ctk.CTkButton(self.buttons_frame, image=self.icon_save, compound="left", text="Enregistrer l'image", width=200, fg_color="#146409",hover_color="#1E8810", text_color="white",border_color="#317E27", border_width=2 )
+        self.save_button.grid(row=1, column=1, padx=10)
+        
+        
+ 
+        
 
     # ---- Méthodes utilitaires ----
     def get_input_text(self):
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # main.py
 from m.model import CodeGeneratorModel
-from m.v.c.view import CodeGeneratorView
+from m.v.view import CodeGeneratorView
 from m.v.c.controller import CodeController
 
 if __name__ == "__main__":
